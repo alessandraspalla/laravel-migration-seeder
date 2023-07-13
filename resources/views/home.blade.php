f6156a19c07553a2ea997785cf917bd3b4a7e7e6
alessandraspalla <alespalla93@icloud.com>
2023-07-14 00:30:40 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.layout')
+@section('content')
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, nemo modi ut inventore laboriosam illo temporibus voluptatibus id corrupti quam impedit labore, enim porro quo vel recusandae nisi sequi cupiditate.</p>
+@endsection
